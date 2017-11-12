a8773caf7cc5648117a0186a0eb39a6208ba6dc0
Siddharth Godbole <Siddharth_Godbole@symantec.com>
2017-11-12 15:46:25 +0530

    Pointer tests

---
+++ pointers/main.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+func main() {
+	a := 9
+
+	fmt.Println("a = ", a)
+	fmt.Println("address of a = ", &a)
+
+	var b *int = &a
+
+	fmt.Println("value of b = ", b)
+	fmt.Println("value of *b = ", *b)
+
+	var c **int = &b
+
+	fmt.Println("value of c = ", c)
+	fmt.Println("value of *c = ", *c)
+}
