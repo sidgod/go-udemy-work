7f86b5c66435e156241fc8204353775fb8b05ad0
Siddharth Godbole <Siddharth_Godbole@symantec.com>
2017-11-12 12:28:42 +0530

    Closure tests

---
+++ closure/main.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+func wrapper() func() int {
+	x := 0
+	return func() int {
+		x++
+		return x
+	}
+}
+
+func main()  {
+	increment := wrapper()
+	fmt.Println(increment())
+	fmt.Println(increment())
+	increment = wrapper()
+	fmt.Println(increment())
+}
